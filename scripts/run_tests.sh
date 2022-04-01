bbd1eccb641a450766cc02298432df0995e3efe9
Alexander Bogdanov <a@bogdanov.co>
2022-04-01 19:22:43 +0400

    🧑‍💻 Add deploy and test scripts

---
+++ scripts/run_tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+tmpfile=$(mktemp /tmp/docker-compose.XXXXXX.yml)
+sh -s "https://raw.githubusercontent.com/SphericalPotatoInVacuum/soa-message-queues/main/deploy/docker-compose.yml" > "$tmpfile"
+
+docker-compose -f "$tmpfile" --profile client pull
+docker-compose -f "$tmpfile" up -d && docker-compose -f "$tmpfile" run --rm -it client || true
+docker-compose -f "$tmpfile" down
+
+rm "$tmpfile"
